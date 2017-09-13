fbfd6f49368b9910d3a6728168ae501a64f36243
wassimhazime <wassim.hazime@gmail.com>
2017-09-13 15:39:52 +0200

    Update jquery.quicksearch.js

--- jquery.quicksearch.js
+++ jquery.quicksearch.js
@@ -1,181 +1,185 @@
-(function($, window, document, undefined) {
-	$.fn.quicksearch = function (target, opt) {
-		
-		var timeout, cache, rowcache, jq_results, val = '', e = this, options = $.extend({ 
-			delay: 100,
-			selector: null,
-			stripeRows: null,
-			loader: null,
-			noResults: '',
-			matchedResultsCount: 0,
-			bind: 'keyup',
-			onBefore: function () { 
-				return;
-			},
-			onAfter: function () { 
-				return;
-			},
-			show: function () {
-				this.style.display = "";
-			},
-			hide: function () {
-				this.style.display = "none";
-			},
-			prepareQuery: function (val) {
-				return val.toLowerCase().split(' ');
-			},
-			testQuery: function (query, txt, _row) {
-				for (var i = 0; i < query.length; i += 1) {
-					if (txt.indexOf(query[i]) === -1) {
-						return false;
-					}
-				}
-				return true;
-			}
-		}, opt);
-		
-		this.go = function () {
-			
-			var i = 0,
-				numMatchedRows = 0,
-				noresults = true, 
-				query = options.prepareQuery(val),
-				val_empty = (val.replace(' ', '').length === 0);
-			
-			for (var i = 0, len = rowcache.length; i < len; i++) {
-				if (val_empty || options.testQuery(query, cache[i], rowcache[i])) {
-					options.show.apply(rowcache[i]);
-					noresults = false;
-					numMatchedRows++;
-				} else {
-					options.hide.apply(rowcache[i]);
-				}
-			}
-			
-			if (noresults) {
-				this.results(false);
-			} else {
-				this.results(true);
-				this.stripe();
-			}
-			
-			this.matchedResultsCount = numMatchedRows;
-			this.loader(false);
-			options.onAfter();
-			
-			return this;
-		};
-		
-		/*
-		 * External API so that users can perform search programatically. 
-		 * */
-		this.search = function (submittedVal) {
-			val = submittedVal;
-			e.trigger();
-		};
-		
-		/*
-		 * External API to get the number of matched results as seen in 
-		 * https://github.com/ruiz107/quicksearch/commit/f78dc440b42d95ce9caed1d087174dd4359982d6
-		 * */
-		this.currentMatchedResults = function() {
-			return this.matchedResultsCount;
-		};
-		
-		this.stripe = function () {
-			
-			if (typeof options.stripeRows === "object" && options.stripeRows !== null)
-			{
-				var joined = options.stripeRows.join(' ');
-				var stripeRows_length = options.stripeRows.length;
-				
-				jq_results.not(':hidden').each(function (i) {
-					$(this).removeClass(joined).addClass(options.stripeRows[i % stripeRows_length]);
+(function ($) {
+    class Quicksearch {
+        constructor(selector, target, options) {
+
+            this.selector = selector;
+            this.target = target;
+            this.timeout = 110;
+            this.cache = '';
+            this.rowcache = "";
+            this.jq_results = "";
+            this.val = '';
+            this.$this = this;
+            this.options = options;
+
+
+
+        }
+
+                init() {
+            this.cacheF();// chaque  select convert to string ( strip_html )stocker dans variable rowcach et run go()
+            this.results(true);
+            this.stripe();
+            this.loader(false);
+        }
+        ;
+                trigger() {
+            this.loader(true); // charge logo gif
+            this.options.onBefore(); // charg function
+
+            window.clearTimeout(this.timeout);
+            var $this = this;
+            this.timeout = window.setTimeout(function () {
+                $this.go();
+            }, this.options.delay);
+
+            return this;
+        }
+        ;
+                go() {
+
+            var i = 0,
+                    numMatchedRows = 0,
+                    noresults = true,
+                    query = this.options.prepareQuery(this.val), // val to array avec toLowerCase
+                    val_empty = (this.val.replace(' ', '').length === 0);
+
+            for (var i = 0, len = this.rowcache.length; i < len; i++) { // chaque elemant de list qui select
+                if (val_empty || this.options.testQuery(query, this.cache[i], this.rowcache[i])) {
+                    this.options.show.apply(this.rowcache[i]);
+                    noresults = false;
+                    numMatchedRows++;
+                } else {
+                    this.options.hide.apply(this.rowcache[i]);
+                }
+            }
+
+            if (noresults) {
+                this.results(false, numMatchedRows);
+            } else {
+                this.results(true, numMatchedRows);
+                this.stripe();
+            }
+
+            this.loader(false);
+            this.options.onAfter();
+
+            return this.selector;
+        }
+        ;
+                stripe() {
+
+            if (typeof this.options.stripeRows === "object" && this.options.stripeRows !== null)
+            {
+                var joined = this.options.stripeRows.join(' ');
+                var stripeRows_length = this.options.stripeRows.length;
+
+			this.jq_results.not(':hidden').each(function (i) {
+				$(this).removeClass(joined).addClass(this.options.stripeRows[i % stripeRows_length]);
 				});
-			}
-			
-			return this;
-		};
-		
-		this.strip_html = function (input) {
-			var output = input.replace(new RegExp('<[^<]+\>', 'g'), "");
-			output = $.trim(output.toLowerCase());
-			return output;
-		};
-		
-		this.results = function (bool) {
-			if (typeof options.noResults === "string" && options.noResults !== "") {
-				if (bool) {
-					$(options.noResults).hide();
-				} else {
-					$(options.noResults).show();
-				}
-			}
-			return this;
-		};
-		
-		this.loader = function (bool) {
-			if (typeof options.loader === "string" && options.loader !== "") {
-				 (bool) ? $(options.loader).show() : $(options.loader).hide();
-			}
-			return this;
-		};
-		
-		this.cache = function () {
-			
-			jq_results = $(target);
-			
-			if (typeof options.noResults === "string" && options.noResults !== "") {
-				jq_results = jq_results.not(options.noResults);
-			}
-			
-			var t = (typeof options.selector === "string") ? jq_results.find(options.selector) : $(target).not(options.noResults);
-			cache = t.map(function () {
-				return e.strip_html(this.innerHTML);
-			});
-			
-			rowcache = jq_results.map(function () {
-				return this;
-			});
 
-			/*
-			 * Modified fix for sync-ing "val". 
-			 * Original fix https://github.com/michaellwest/quicksearch/commit/4ace4008d079298a01f97f885ba8fa956a9703d1
-			 * */
-			val = val || this.val() || "";
-			
-			return this.go();
-		};
-		
-		this.trigger = function () {
-			this.loader(true);
-			options.onBefore();
-			
-			window.clearTimeout(timeout);
-			timeout = window.setTimeout(function () {
-				e.go();
-			}, options.delay);
-			
-			return this;
-		};
-		
-		this.cache();
-		this.results(true);
-		this.stripe();
-		this.loader(false);
-		
-		return this.each(function () {
-			
-			/*
-			 * Changed from .bind to .on.
-			 * */
-			$(this).on(options.bind, function () {
-				
-				val = $(this).val();
-				e.trigger();
-			});
-		});
-		
-	};
+            }
 
-}(jQuery, this, document));
+            return this;
+        }
+        ;
+                results(bool, numMatchedRows) {
+            if (typeof this.options.noResults === "string" && this.options.noResults !== "") {
+                if (bool) {
+                    $(this.options.noResults).hide();
+                } else {
+                    $(this.options.noResults).show();
+                }
+            }
+
+            if (typeof this.options.totalResults === "string" && this.options.totalResults !== "") {
+                $(this.options.totalResults).html(numMatchedRows);
+            }
+            return this;
+        }
+        ;
+                loader(bool) { // logo loader.gif
+            if (typeof this.options.loader === "string" && this.options.loader !== "") {
+                (bool) ? $(this.options.loader).show() : $(this.options.loader).hide();
+            }
+            return this;
+        }
+        ;
+                cacheF() {
+
+
+            this.jq_results = $(this.target);
+
+            if (typeof this.options.noResults === "string" && this.options.noResults !== "") {
+                this.jq_results = this.jq_results.not(this.options.noResults);
+            }
+
+            var t = (typeof this.options.selector === "string") ? this.jq_results.find(this.options.selector) : $(this.target).not(this.options.noResults);
+            this.cache = t.map(function () {
+                return $.trim((this.innerHTML).replace(new RegExp('<[^<]+\>', 'g'), "").toLowerCase());
+
+            });
+
+            this.rowcache = this.jq_results.map(function () {
+                return this;
+            });
+
+            return this.go();  // val defauld
+        }
+        ;
+    }
+
+
+
+
+    $.fn.quicksearch = function (target, opt) {
+
+
+        var options = $.extend({
+            delay: 100,
+            selector: null,
+            stripeRows: null,
+            loader: null,
+            noResults: '',
+            totalResults: '#total',
+            bind: 'keyup',
+            onBefore: function () {
+                return;
+            },
+            onAfter: function () {
+                return;
+            },
+            show: function () {
+                this.style.display = "";
+            },
+            hide: function () {
+                this.style.display = "none";
+            },
+            prepareQuery: function (val) {
+                return val.toLowerCase().split(' ');
+            },
+            testQuery: function (query, txt, _row) {
+                for (var i = 0; i < query.length; i++) {
+                    if (txt.indexOf(query[i]) === -1) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }, opt);
+
+
+        var quicksearchq = new Quicksearch(this, target, options)
+             quicksearchq.init()
+
+        var fin = this.each(function () {
+            $(this).on('keyup'  // event keyup
+                    , function () {
+                        quicksearchq.val = $(this).val();
+                        quicksearchq.trigger();
+                    });
+        });
+        return fin
+    };
+
+}(jQuery));
